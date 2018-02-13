f37ac9aca1b7395959067f34d82f8a5ecbda6775
Spencer Carli <spencercarli@gmail.com>
2018-02-13 13:28:02 -0600

    Upgrade deps

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ export default class SplashScreenDemo extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.js
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
