58267f0ea7f38235a8ab3a588232d7bceacbb284
Spencer Carli <spencercarli@gmail.com>
2018-02-13 14:37:52 -0600

    Setup android

--- android/app/src/main/java/com/splashscreendemo/MainActivity.java
+++ android/app/src/main/java/com/splashscreendemo/MainActivity.java
@@ -1,8 +1,15 @@
 package com.splashscreendemo;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
